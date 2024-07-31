eb89a10306456719133776b17aac6f09d3739e12
Maximiliano Duran <maximiliano.duran114@gmail.com>
2024-07-31 03:23:56 -0400

    pls

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Maxi-Duran/SnakeGame-React/",
+  base: "https://maxi-duran.github.io/SnakeGame-React/",
 });
